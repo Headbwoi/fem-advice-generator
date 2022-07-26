16cba257b1b85c6f595d13018138d466c839d2b7
headbwoi <tochukwujohn24@gmail.com>
2022-07-26 11:55:15 +0100

    finished setup

---
+++ tailwind.config.cjs
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./main.js"],
+  theme: {
+    extend: {
+      colors: {
+        Light_Cyan: "hsl(193, 38%, 86%)",
+        Neon_Green: "hsl(150, 100%, 66%)",
+        Grayish_Blue: "hsl(217, 19%, 38%)",
+        Dark_Grayish_Blue: "hsl(217, 19%, 24%)",
+        Dark_Blue: "hsl(218, 23%, 16%)",
+      },
+    },
+  },
+  plugins: [],
+}

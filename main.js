0c38707969426896c9b4dc2026f45a7eb3b3cf92
headbwoi <tochukwujohn24@gmail.com>
2022-07-27 03:18:56 +0100

    finished

--- main.js
+++ main.js
@@ -1,1 +1,36 @@
-import './style.css'
+import "./style.css"
+
+const adviceCount = document.querySelector("#advice-count")
+const textEl = document.querySelector(".text")
+const iconBtn = document.querySelector(".icon-btn")
+
+let adviceCountNumber = 0
+
+const getData = async () => {
+  const quote = await fetch("https://api.adviceslip.com/advice")
+  const { slip } = await quote.json()
+
+  const advice = slip.advice
+
+  const paragraph = document.createElement("p")
+  paragraph.classList.add(
+    "text-[28px]",
+    "text-Light_Cyan",
+    "font-extrabold",
+    "animate-fade-in"
+  )
+  paragraph.innerText = `${advice}`
+  textEl.appendChild(paragraph)
+
+  if (quote.status === 200) {
+    adviceCountNumber++
+    adviceCount.innerText = `#${adviceCountNumber}`
+  }
+}
+
+iconBtn.addEventListener("click", () => {
+  const paragraph = textEl.querySelector("p")
+  paragraph.remove()
+  getData()
+})
+window.onload = () => getData()
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,6 +5,13 @@ module.exports = {
     container: {
       center: true,
     },
+    colors: {
+      Light_Cyan: "hsl(193, 38%, 86%)",
+      Neon_Green: "hsl(150, 100%, 66%)",
+      Grayish_Blue: "hsl(217, 19%, 38%)",
+      Dark_Grayish_Blue: "hsl(217, 19%, 24%)",
+      Dark_Blue: "hsl(218, 23%, 16%)",
+    },
     screens: {
       sm: "640px",
       md: "768px",
@@ -13,12 +20,21 @@ module.exports = {
       sans: ["'Manrope', 'sans_serif'"],
     },
     extend: {
-      colors: {
-        Light_Cyan: "hsl(193, 38%, 86%)",
-        Neon_Green: "hsl(150, 100%, 66%)",
-        Grayish_Blue: "hsl(217, 19%, 38%)",
-        Dark_Grayish_Blue: "hsl(217, 19%, 24%)",
-        Dark_Blue: "hsl(218, 23%, 16%)",
+      boxShadow: {
+        "icon-shadow": "0px 0px 20px 13px hsla(150, 100%, 66%, 0.4)",
+      },
+      animation: {
+        "fade-in": "fade-in 2s ease-in-out forwards",
+      },
+      keyframes: {
+        "fade-in": {
+          "0%": {
+            opacity: "0",
+          },
+          "100%": {
+            opacity: "1",
+          },
+        },
       },
     },
   },
